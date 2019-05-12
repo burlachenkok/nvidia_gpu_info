b1b1b7404c45596cf40ed31f1b814f21a8113625
bruziuz <burlachenkok@gmail.com>
2019-05-12 19:08:55 +0300

    update

--- build_windows.bat
+++ build_windows.bat
@@ -1,10 +1,11 @@
 :: Build in Windows
 :: Import Visual C++ 2013 toolchain, etc.
 ::-----------------------------------------------------------------
-set VS_DIR=%VS120COMNTOOLS:~,-15%\VC
-set PATH=%PATH%;%VS_DIR%
-call "%VS_DIR%/vcvarsall.bat" x86
+::set VS_DIR=%VS120COMNTOOLS:~,-15%\VC
+::set PATH=%PATH%;%VS_DIR%
+::call "%VS_DIR%/vcvarsall.bat" x86
 ::-----------------------------------------------------------------
-nvcc nvidia_gpu_info.cu -o nvidia_gpu_info
-./nvidia_gpu_info
+nvcc nvidia_gpu_info.cu -o nvidia_gpu_info -ccbin "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Tools\MSVC\14.10.25017\bin\HostX64\x64" 
+nvidia_gpu_info
+
 
